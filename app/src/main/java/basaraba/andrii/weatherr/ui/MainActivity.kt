12cd569c32df5cbdfa96a54ac157d71ab7d3f4ad
Andrii Basaraba <apollon.trnpl@gmail.com>
2019-04-08 12:47:43 +0300

    upd - color of status bar

--- app/src/main/java/basaraba/andrii/weatherr/ui/MainActivity.kt
+++ app/src/main/java/basaraba/andrii/weatherr/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import basaraba.andrii.weatherr.CITY_WEATHER
 import basaraba.andrii.weatherr.R
 import basaraba.andrii.weatherr.getColorFromAttr
@@ -23,9 +24,13 @@ class MainActivity : AppCompatActivity() {
             if (Math.abs(p1) - p0!!.totalScrollRange == 0) {
                 tvToolbar.setTextColor(getColorFromAttr(R.attr.colorPrimary))
                 toolbarMain.setBackgroundColor(Color.WHITE)
+                window.statusBarColor = Color.WHITE
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             } else {
                 tvToolbar.setTextColor(Color.WHITE)
                 toolbarMain.setBackgroundColor(getColorFromAttr(R.attr.colorPrimary))
+                window.statusBarColor = getColorFromAttr(R.attr.colorPrimary)
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             }
         })
     }
